f7e56e9820149aeffc033f83cba1ebfa593f9aa8
OXIG <126681352+OXIG@users.noreply.github.com>
2026-04-05 23:48:30 +0300

    Update __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,5 @@
+from .start import router as start_router
+from .balance import router as balance_router
+from .payment import router as payment_router
+from .generate import router as generate_router
+from .admin import router as admin_router
